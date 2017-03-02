b882a2582257bd3b58f4e9846fbaa770dd82fe93
Matheus Evangelista <matheus.evangelista@founders.com.br>
2017-03-02 15:48:06 -0300

    adicionando relacionamento via model

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,4 +4,15 @@
 
   @include('posts.post')
 
+  <hr/>
+
+  <div class="comments">
+    @foreach($post->comments as $comment)
+        <article>
+            <strong>{{ $comment->created_at->diffForHumans() }}</strong>
+            {{ $comment->body }}
+        </article>
+    @endforeach
+  </div>
+
 @endsection
